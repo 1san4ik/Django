b626e3b97a6317f37dc37ed4a4e05d2154de423d
1san4ik <1san4ik@rambler.ru>
2021-08-31 16:41:39 +0300

    Django Site

--- teboil/third_app/models.py
+++ teboil/third_app/models.py
@@ -1,14 +1,46 @@
+import slug as slug
 from django.db import models
 
 
-class Task(models.Model):
+class Category(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='Название категории')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
     objects = None
-    title = models.CharField('Название', max_length=35)
-    text = models.TextField('Описание')
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='Название товара', verbose_name_plural='Названия Товаров', max_length=35)
+    text = models.TextField(verbose_name='Описание', null=True)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='Изображение')
+    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
 
     def __str__(self):
         return self.title
 
-    class Meta:
-        verbose_name = 'Название Товара'
-        verbose_name_plural = 'Названия Товаров'
+
+class CartProduct(models.Model):
+
+    user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
+    qty = models.PositiveIntegerField(default=1)
+    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
+
+    def __str__(self):
+        return 'Продукт {} (для корзины):'.format(self.product.title)
+
+
+class Cart(models.Model):
+
+    owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)
+    products = models.ManyToManyField(CartProduct, )
+
+
+
